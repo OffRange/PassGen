f1e5e53382d57144e2720c7578108d864c02e26a
Davis <davis-wolfermann2005@gmx.de>
2024-04-14 20:47:15 +0200

    [Documentation] Added documentation

--- src/main/kotlin/de/davis/passgen/PasswordGenerator.kt
+++ src/main/kotlin/de/davis/passgen/PasswordGenerator.kt
@@ -6,6 +6,11 @@ import java.security.SecureRandom
 import kotlin.random.Random
 import kotlin.random.asKotlinRandom
 
+/**
+ * DSL-driven password generator.
+ * @property random The random number generator to use for password generation.
+ * Defaults to a secure random number generator.
+ */
 @PasswordGeneratorDsl
 class PasswordGenerator(private val random: Random = SecureRandom().asKotlinRandom()) {
 
@@ -13,30 +18,53 @@ class PasswordGenerator(private val random: Random = SecureRandom().asKotlinRand
     private var characterSet: MutableSet<CharacterSet> = mutableSetOf()
 
 
+    /**
+     * Sets the length of the generated password.
+     * @param length The length of the password.
+     */
     fun length(length: Int) {
         this.length = length
     }
 
+    /**
+     * Adds lowercase letters to the character set.
+     */
     fun lowercase() {
         characterSet += CharacterSet.Lower
     }
 
+    /**
+     * Adds uppercase letters to the character set.
+     */
     fun uppercase() {
         characterSet += CharacterSet.Upper
     }
 
+    /**
+     * Adds digits to the character set.
+     */
     fun digits() {
         characterSet += CharacterSet.Digits
     }
 
+    /**
+     * Adds punctuation characters to the character set.
+     */
     fun punctuations() {
         characterSet += CharacterSet.Punctuations
     }
 
+    /**
+     * Allows adding a custom character set to the password generator.
+     * @param custom A lambda function to customize the character set.
+     */
     fun custom(custom: (MutableSet<CharacterSet>) -> Unit) {
         custom(characterSet)
     }
 
+    /**
+     * Adds lowercase letters, uppercase letters, digits, and punctuations to the character set.
+     */
     fun alphanumeric() {
         lowercase()
         uppercase()
@@ -44,6 +72,10 @@ class PasswordGenerator(private val random: Random = SecureRandom().asKotlinRand
         punctuations()
     }
 
+    /**
+     * Generates a password based on the configured settings.
+     * @return The generated password along with the used character sets.
+     */
     fun generate(): GeneratedPassword {
         val generated = (1..length).map {
             val charSet = characterSet.random(random)
@@ -56,6 +88,15 @@ class PasswordGenerator(private val random: Random = SecureRandom().asKotlinRand
     }
 }
 
+/**
+ * DSL function to generate a password using the PasswordGenerator DSL.
+ *
+ * @param init The lambda function used to configure the password generator.
+ * Within this lambda, you can specify desired characteristics of the generated password
+ * using functions like `lowercase()`, `uppercase()`, `digits()`, `punctuations()`, `custom()`, or `alphanumeric()`.
+ *
+ * @return A [GeneratedPassword] object representing the generated password along with the set of character sets used in its generation.
+ */
 @PasswordGeneratorDsl
 fun generatePassword(init: PasswordGenerator.() -> Unit): GeneratedPassword {
     val generator = PasswordGenerator()
--- src/main/kotlin/de/davis/passgen/characterset/CharacterSet.kt
+++ src/main/kotlin/de/davis/passgen/characterset/CharacterSet.kt
@@ -1,25 +1,48 @@
 package de.davis.passgen.characterset
 
+/**
+ * Interface representing a character set used in password generation.
+ */
 sealed interface CharacterSet {
 
+    /**
+     * Retrieves the characters belonging to the character set.
+     * @return A string containing the characters of the set.
+     */
     fun getChars(): String
 
+    /**
+     * Represents the set of lowercase letters (a-z).
+     */
     data object Lower : CharacterSet {
         override fun getChars(): String = ('a'..'z').joinToString("")
     }
 
+    /**
+     * Represents the set of uppercase letters (A-Z).
+     */
     data object Upper : CharacterSet {
         override fun getChars(): String = ('A'..'Z').joinToString("")
     }
 
+    /**
+     * Represents the set of digits (0-9).
+     */
     data object Digits : CharacterSet {
         override fun getChars(): String = ('0'..'9').joinToString("")
     }
 
+    /**
+     * Represents a set of common punctuation characters.
+     */
     data object Punctuations : CharacterSet {
         override fun getChars(): String = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
     }
 
+    /**
+     * Represents a custom character set defined by a specific string of characters.
+     * @property characters The custom characters for the set.
+     */
     data class Custom(val characters: String) : CharacterSet {
         override fun getChars(): String = characters
     }
